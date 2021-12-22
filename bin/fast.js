8b048b66213a847af1da927ff71f682981edd074
duchengdong <xinyiyake006@163.com>
2021-12-22 10:48:37 +0800

    添加版本号

--- bin/fast.js
+++ bin/fast.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 const program = require('commander')
-program.version('1.3.1')
+program.version('1.3.2')
     .usage('<command> [项目名称]')
     .command('init', '初始化项目')
     .parse(process.argv)
