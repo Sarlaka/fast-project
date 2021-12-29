b519e4147f71f3b4c80d1f6d91d087a6955412df
duchengdong <xinyiyake006@163.com>
2021-12-29 16:55:11 +0800

    修改项目git地址,添加react+ts项目模板

--- bin/fast.js
+++ bin/fast.js
@@ -1,6 +1,3 @@
 #!/usr/bin/env node
 const program = require('commander')
-program.version('1.3.2')
-    .usage('<command> [项目名称]')
-    .command('init', '初始化项目')
-    .parse(process.argv)
+program.version('1.3.3').usage('<command> [项目名称]').command('init', '初始化项目').parse(process.argv)
